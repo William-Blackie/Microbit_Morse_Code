2a086465a4130d8a93ab731a0b2fdb59c029f478
Christopher Taylor <christopher15.taylor@live.uwe.ac.uk>
2018-02-26 14:37:28 +0000

    Combo.

--- Receive/main.cpp
+++ Receive/main.cpp
@@ -1,14 +1,41 @@
-
 #include "MicroBit.h"
 #include <queue>
-
+#include<unordered_map>
 
 MicroBitPin P0(MICROBIT_ID_IO_P0, MICROBIT_PIN_P0, PIN_CAPABILITY_ALL);
 MicroBit uBit;
 int EPOCH = 15;
 uint16_t currentTime;
 std::queue<char> charQueue;
+std::unordered_multimap<std::string, char> Morse;
 
+
+std::string concatenate() {
+    char charArray[5];
+    std::string result;
+
+    for (std::size_t i = 0; i < 5; i++) {
+        charArray[i] = charQueue.front();
+        charQueue.pop();
+        result = result + charArray[i];
+    }
+    printf("%s",result);
+    return result;
+
+//    uBit.display.scroll(result);
+}
+
+
+char translate(std::string morse) {
+    auto iterator = Morse.find(morse);
+    if (iterator == Morse.end()) {
+        return 'f';
+    }
+//    uBit.display.scroll(iterator->second);
+//  printf("Your morse is: %c", iterator->second);
+    return iterator->second;
+
+}
 
 
 
@@ -38,17 +65,17 @@ void receiveSignal(){
         uint16_t delta = system_timer_current_time() - currentTime;
 
         if(pinOn){
-                if (delta > 150) {
-                    charQueue.push('-');
-                    uBit.sleep(500);
-                    pinOn = false;
-                }
+            if (delta > 150) {
+                charQueue.push('-');
+                uBit.sleep(500);
+                pinOn = false;
+            }
 
-                else if (delta <= 50) {
-                    charQueue.push('.');
-                    uBit.sleep(500);
-                    pinOn = false;
-                }
+            else if (delta <= 50) {
+                charQueue.push('.');
+                uBit.sleep(500);
+                pinOn = false;
+            }
 
         }
     }
@@ -64,22 +91,58 @@ void printAll(std::queue<char> queue){
 }
 
 
+
+
 int main()
 {
     // Initialise the micro:bit runtime.
     uBit.init();
 
-//    printAll(charQueue);
-    receiveSignal();
-    printAll(charQueue);
-//    charQueue.push('-');
-//    charQueue.push('.');
+    Morse.insert(std::make_pair(".-",'A'));
+    Morse.insert(std::make_pair("-...",'B'));
+    Morse.insert(std::make_pair("-.-.",'C'));
+    Morse.insert(std::make_pair("-..",'D'));
+    Morse.insert(std::make_pair(".",'E'));
+    Morse.insert(std::make_pair("..-.",'F'));
+    Morse.insert(std::make_pair("--.",'G'));
+    Morse.insert(std::make_pair("....",'H'));
+    Morse.insert(std::make_pair("..",'I'));
+    Morse.insert(std::make_pair(".---",'J'));
+    Morse.insert(std::make_pair("-.-",'K'));
+    Morse.insert(std::make_pair(".-..",'L'));
+    Morse.insert(std::make_pair("--",'M'));
+    Morse.insert(std::make_pair("-.",'N'));
+    Morse.insert(std::make_pair("---",'O'));
+    Morse.insert(std::make_pair(".--.",'P'));
+    Morse.insert(std::make_pair("--.",'Q'));
+    Morse.insert(std::make_pair(".-.",'R'));
+    Morse.insert(std::make_pair("...",'S'));
+    Morse.insert(std::make_pair("-",'T'));
+    Morse.insert(std::make_pair("..-",'U'));
+    Morse.insert(std::make_pair("...-",'V'));
+    Morse.insert(std::make_pair(".--",'W'));
+    Morse.insert(std::make_pair("-..-",'X'));
+    Morse.insert(std::make_pair("-.--",'Y'));
+    Morse.insert(std::make_pair("--..",'Z'));
+    Morse.insert(std::make_pair(".----",'1'));
+    Morse.insert(std::make_pair("..---",'2'));
+    Morse.insert(std::make_pair("...--",'3'));
+    Morse.insert(std::make_pair("....-",'4'));
+    Morse.insert(std::make_pair(".....",'5'));
+    Morse.insert(std::make_pair("-....",'6'));
+    Morse.insert(std::make_pair("--...",'7'));
+    Morse.insert(std::make_pair("---..",'8'));
+    Morse.insert(std::make_pair("----.",'9'));
+    Morse.insert(std::make_pair("-----",'0'));
 
+//should be G
+    /*charQueue.push('-');
+    charQueue.push('-');
+    charQueue.push('.');*/
 
+    charQueue.push('--.');
 
-    // If main exits, there may still be other fibers running or registered event handlers etc.
-    // Simply release this fiber, which will mean we enter the scheduler. Worse case, we then
-    // sit in the idle task forever, in a power efficient sleep.
+    uBit.display.scroll(translate(concatenate()));
+
     release_fiber();
 }
-
