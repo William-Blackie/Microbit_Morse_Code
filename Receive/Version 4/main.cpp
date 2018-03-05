d474874a7a15513add4f75a459353db8eae44078
Christopher Taylor <christopher15.taylor@live.uwe.ac.uk>
2018-03-05 10:38:32 +0000

    Update main.cpp

--- Receive/Version 4/main.cpp
+++ Receive/Version 4/main.cpp
@@ -1,26 +1,23 @@
 #include "MicroBit.h"
 #include <queue>
 #include<unordered_map>
+#include <string>
 
 
-MicroBitPin P0(MICROBIT_ID_IO_P0, MICROBIT_PIN_P0, PIN_CAPABILITY_ALL);
+
 MicroBit uBit;
-int EPOCH = 15;
-uint16_t currentTime;
+MicroBitPin P0(MICROBIT_ID_IO_P0, MICROBIT_PIN_P0, PIN_CAPABILITY_ALL);
+MicroBitSerial serial(USBTX, USBRX);
 std::queue<char> charQueue;
 std::unordered_multimap<std::string, char> Morse;
-MicroBitSerial serial(USBTX, USBRX);
-
-
 
 std::string concatenate() {
-    char charArray[5];
-//    std::string result;
-    for (std::size_t i = 0; i < 5; i++) {
-        charArray[i] = charQueue.front();
+    std::string result = "";
+    while (!charQueue.empty()) {
+        result.push_back(charQueue.front());
         charQueue.pop();
     }
-    return charArray;
+    return result;
 }
 
 
@@ -33,46 +30,107 @@ char translate(std::string morse) {
 
 }
 
+//void checkConnection() {
+//    while (true) {
+//        if (uBit.io.P0.getDigitalValue() != 0) {
+//            uBit.display.scroll("y");
+//        } else {
+//            uBit.display.scroll("n");
+//        }
+//    }
+//}
 
-
-void checkConnection(){
-    while(true){
-        if(P0.getDigitalValue() !=0){
-            uBit.display.scroll("y");
-        }else{
-            uBit.display.scroll("n");
-        }
-    }
-}
-
-
-
-void receiveSignal(){
+/*void receiveSignal() {
     bool pinOn = false;
-    while (charQueue.size() <= 6){
-//        uBit.display.scroll("hi");
-        //read current number of milliseconds
-        currentTime = (int)uBit.systemTime();
+    bool timeOut = false;
 
-        while (P0.getDigitalValue() != 0){
+    while (!timeOut) {
+        uint64_t currentTime = system_timer_current_time();
+        //read current number of milliseconds
+        while (P0.getDigitalValue() != 0) {
             pinOn = true;
         }
+
         //actual time for dot or dash.
-        uint16_t delta = system_timer_current_time() - currentTime;
+        uint64_t delta = system_timer_current_time() - currentTime;
 
         if(pinOn){
-            if (delta > 150) {
+            if (delta > 100) {
                 charQueue.push('-');
-                uBit.sleep(500);
-                pinOn = false;
-            }
-
-            else if (delta <= 50) {
+                uBit.sleep(45);
+            } else if (delta > 50) {
                 charQueue.push('.');
-                uBit.sleep(500);
-                pinOn = false;
+                uBit.sleep(45);
             }
+        }
+        else{
+            pinOn = false;
+            timeOut= true;
+            uBit.display.print('a');
+        }
 
+    }
+}*/
+
+void receiveSignal() {
+
+//    bool pinOn = false;
+//
+//    while (P0.getDigitalValue()) {
+//        uint64_t currentTime = system_timer_current_time();
+//        //read current number of milliseconds
+//        while (P0.getDigitalValue() == 1) {
+//            pinOn = true;
+//        }
+//
+//        //actual time for dot or dash.
+//        uint64_t delta = system_timer_current_time() - currentTime;
+//
+//        if (delta > 100) {
+//            charQueue.push('-');
+//            uBit.sleep(49);
+//            pinOn = false;
+//            uBit.display.print("-");
+//        } else if (delta > 10) {
+//            charQueue.push('.');
+//            uBit.sleep(49);
+//            pinOn = false;
+//            uBit.display.print(".");
+//        }
+//        else{
+//            uBit.display.print("b");
+//        }
+//    }
+
+    Timer timer;
+    timer.start();
+
+    while (true) {
+        timer.reset();
+
+        while (P0.getDigitalValue() == 0) {
+            if (timer.read_ms() > 450) {
+                return;
+            }
+            uBit.sleep(1);
+        }
+
+        timer.reset();
+
+        while (P0.getDigitalValue() == 1) {
+            uBit.sleep(1);
+        }
+
+        int duration = timer.read_ms();
+
+        if (duration < 100) {
+            charQueue.push('.');
+            uBit.display.print(".");
+        } else if (duration < 200) {
+            charQueue.push('-');
+            uBit.display.print("-");
+        } else {
+            return;
         }
     }
 }
@@ -89,61 +147,65 @@ void receiveSignal(){
 
 
 
-int main()
-{
+int main() {
     // Initialise the micro:bit runtime.
     uBit.init();
 
-    Morse.insert(std::make_pair(".-",'A'));
-    Morse.insert(std::make_pair("-...",'B'));
-    Morse.insert(std::make_pair("-.-.",'C'));
-    Morse.insert(std::make_pair("-..",'D'));
-    Morse.insert(std::make_pair(".",'E'));
-    Morse.insert(std::make_pair("..-.",'F'));
-    Morse.insert(std::make_pair("--.",'G'));
-    Morse.insert(std::make_pair("....",'H'));
-    Morse.insert(std::make_pair("..",'I'));
-    Morse.insert(std::make_pair(".---",'J'));
-    Morse.insert(std::make_pair("-.-",'K'));
-    Morse.insert(std::make_pair(".-..",'L'));
-    Morse.insert(std::make_pair("--",'M'));
-    Morse.insert(std::make_pair("-.",'N'));
-    Morse.insert(std::make_pair("---",'O'));
-    Morse.insert(std::make_pair(".--.",'P'));
-    Morse.insert(std::make_pair("--.-",'Q'));
-    Morse.insert(std::make_pair(".-.",'R'));
-    Morse.insert(std::make_pair("...",'S'));
-    Morse.insert(std::make_pair("-",'T'));
-    Morse.insert(std::make_pair("..-",'U'));
-    Morse.insert(std::make_pair("...-",'V'));
-    Morse.insert(std::make_pair(".--",'W'));
-    Morse.insert(std::make_pair("-..-",'X'));
-    Morse.insert(std::make_pair("-.--",'Y'));
-    Morse.insert(std::make_pair("--..",'Z'));
-    Morse.insert(std::make_pair(".----",'1'));
-    Morse.insert(std::make_pair("..---",'2'));
-    Morse.insert(std::make_pair("...--",'3'));
-    Morse.insert(std::make_pair("....-",'4'));
-    Morse.insert(std::make_pair(".....",'5'));
-    Morse.insert(std::make_pair("-....",'6'));
-    Morse.insert(std::make_pair("--...",'7'));
-    Morse.insert(std::make_pair("---..",'8'));
-    Morse.insert(std::make_pair("----.",'9'));
-    Morse.insert(std::make_pair("-----",'0'));
-//    Morse.insert(std::make_pair(" ",' '));
+    Morse.insert(std::make_pair(".-", 'A'));
+    Morse.insert(std::make_pair("-...", 'B'));
+    Morse.insert(std::make_pair("-.-.", 'C'));
+    Morse.insert(std::make_pair("-..", 'D'));
+    Morse.insert(std::make_pair(".", 'E'));
+    Morse.insert(std::make_pair("..-.", 'F'));
+    Morse.insert(std::make_pair("--.", 'G'));
+    Morse.insert(std::make_pair("....", 'H'));
+    Morse.insert(std::make_pair("..", 'I'));
+    Morse.insert(std::make_pair(".---", 'J'));
+    Morse.insert(std::make_pair("-.-", 'K'));
+    Morse.insert(std::make_pair(".-..", 'L'));
+    Morse.insert(std::make_pair("--", 'M'));
+    Morse.insert(std::make_pair("-.", 'N'));
+    Morse.insert(std::make_pair("---", 'O'));
+    Morse.insert(std::make_pair(".--.", 'P'));
+    Morse.insert(std::make_pair("--.", 'Q'));
+    Morse.insert(std::make_pair(".-.", 'R'));
+    Morse.insert(std::make_pair("...", 'S'));
+    Morse.insert(std::make_pair("-", 'T'));
+    Morse.insert(std::make_pair("..-", 'U'));
+    Morse.insert(std::make_pair("...-", 'V'));
+    Morse.insert(std::make_pair(".--", 'W'));
+    Morse.insert(std::make_pair("-..-", 'X'));
+    Morse.insert(std::make_pair("-.--", 'Y'));
+    Morse.insert(std::make_pair("--..", 'Z'));
+    Morse.insert(std::make_pair(".----", '1'));
+    Morse.insert(std::make_pair("..---", '2'));
+    Morse.insert(std::make_pair("...--", '3'));
+    Morse.insert(std::make_pair("....-", '4'));
+    Morse.insert(std::make_pair(".....", '5'));
+    Morse.insert(std::make_pair("-....", '6'));
+    Morse.insert(std::make_pair("--...", '7'));
+    Morse.insert(std::make_pair("---..", '8'));
+    Morse.insert(std::make_pair("----.", '9'));
+    Morse.insert(std::make_pair("-----", '0'));
 
 
-//should be G
+//    charQueue.push('-');
+//    charQueue.push('-');
+//    charQueue.push('-'); // char o representation
 
-    charQueue.push('-');
-    charQueue.push('-');
-    charQueue.push('.');
-//    charQueue.push(' ');
+    serial.baud(115200);
+    serial.send("test\r\n");
+    while(true){
+        receiveSignal();
+        uBit.display.print(translate(concatenate()));
+        //checkConnection();
+    }
 
 
+//
+//    uBit.display.scroll(translate(".--"));
+//    uBit.display.scroll(translate(concatenate()));
 
 
-//    concatenate();
-    uBit.display.scroll(translate(concatenate()));
     release_fiber();
 }
