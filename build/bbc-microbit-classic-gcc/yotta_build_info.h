4b380a5247e8b462d12bf491b9847bc8fc05994a
William Blackie <william2.blackie@live.uwe.ac.uk>
2018-03-08 18:03:01 +0000

    restructuring folders

--- build/bbc-microbit-classic-gcc/yotta_build_info.h
+++ build/bbc-microbit-classic-gcc/yotta_build_info.h
@@ -2,13 +2,13 @@
 #define __YOTTA_BUILD_INFO_H__
 // yotta build info, #include YOTTA_BUILD_INFO_HEADER to access
 #define YOTTA_BUILD_YEAR 2018 // UTC year
-#define YOTTA_BUILD_MONTH 2 // UTC month 1-12
-#define YOTTA_BUILD_DAY 26 // UTC day 1-31
-#define YOTTA_BUILD_HOUR 14 // UTC hour 0-24
-#define YOTTA_BUILD_MINUTE 18 // UTC minute 0-59
-#define YOTTA_BUILD_SECOND 13 // UTC second 0-61
-#define YOTTA_BUILD_UUID 74b22ac6-1980-4ac9-bfdd-e87923d65a7d // unique random UUID for each build
-#define YOTTA_BUILD_VCS_ID 2ddea794bf0d04f764bdc8c1b0c66c62faf8d4ee // git or mercurial hash
+#define YOTTA_BUILD_MONTH 3 // UTC month 1-12
+#define YOTTA_BUILD_DAY 8 // UTC day 1-31
+#define YOTTA_BUILD_HOUR 18 // UTC hour 0-24
+#define YOTTA_BUILD_MINUTE 1 // UTC minute 0-59
+#define YOTTA_BUILD_SECOND 30 // UTC second 0-61
+#define YOTTA_BUILD_UUID 7445171c-7d5f-449b-8931-688f1c920a46 // unique random UUID for each build
+#define YOTTA_BUILD_VCS_ID 0badb7405482e6702c67a5cb7c88214e65bc21e5 // git or mercurial hash
 #define YOTTA_BUILD_VCS_CLEAN 0 // evaluates true if the version control system was clean, otherwise false
-#define YOTTA_BUILD_VCS_DESCRIPTION 2ddea79 // git describe or mercurial equivalent
+#define YOTTA_BUILD_VCS_DESCRIPTION 0badb74 // git describe or mercurial equivalent
 #endif // ndef __YOTTA_BUILD_INFO_H__
